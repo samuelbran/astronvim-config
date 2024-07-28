b674fddee7475f2d6b116786a5152a64f61ab6bb
Samuel Bran <hello@samuelbran.com>
2024-07-27 20:26:14 -0600

    Initial commit

---
+++ lua/plugins/overriden.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      return require("astrocore").extend_tbl(opts, {
+        window = {
+          position = "right",
+          width = 56,
+        },
+      })
+    end,
+  },
+
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts) opts.winbar = nil end,
+  },
+}
