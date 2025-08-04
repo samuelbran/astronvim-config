1cddf4bc7862e1c19a57d74ec8e441bff7821a0d
Samuel Bran <hello@samuelbran.com>
2025-08-04 09:53:37 -0600

    adjustments for linux

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -81,7 +81,7 @@ return {
         left = { "", "  " },
         right = { "  ", "" },
         center = { "  ", "  " },
-        tab = { "", "" },
+        tab = { " ", " " },
         breadcrumbs = " 󰅂 ",
         path = " 󰅂 ",
       },
--- lua/plugins/overriden.lua
+++ lua/plugins/overriden.lua
@@ -5,8 +5,8 @@ return {
       return require("astrocore").extend_tbl(opts, {
         window = {
           position = "right",
-          -- width = 56,
-          width = 75,
+          width = 46,
+          -- width = 75,
         },
       })
     end,
